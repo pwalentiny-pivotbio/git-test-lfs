2d58660d18e077d1fe89181268aab8592f3cf81c
Patrick Walentiny <patrick.walentiny@walentiny.com>
2024-01-04 23:59:46 -0600

    Refactor lambda_objects_batch_post enable hash checking on upload

--- src/lambda_objects_batch_post.py
+++ src/lambda_objects_batch_post.py
@@ -1,4 +1,5 @@
 ''' API Gateway Integration that implements the LFS Batch API '''
+import base64
 import boto3
 import botocore.exceptions
 
@@ -33,57 +34,86 @@ def handler(event, context):
 
     for _object in objects:
         key = f"{project}/{repo}/{_object['oid']}"
-        Params = {
-            'Bucket': bucket,
-            'Key': key
-        }
-
+        # Git uses sha256 in hex format and S3 uses base64
+        b64_object_oid = _hex_to_base64(_object['oid'])
         response_object = {
             'oid': _object['oid'],
             'size': _object['size'],
             'authenticated': True
         }
 
-        if operation == 'upload':
-            # Git LFS includes a Content-Type header when it uses the basic
-            # trasfer protocol for uplaod operations.  S3 presigned urls
-            # consider this header in the signature so we need to include
-            # it in the parameters.
-            Params['ContentType'] = 'application/octet-stream'
-            client_method = 'put_object'
+        # Check if the object already exists in the S3 object store
+        object_exists = _object_exists(bucket, key)
 
-        elif operation == 'download':
-            client_method = 'get_object'
+        if operation == 'download' and object_exists:
+            url = s3.generate_presigned_url(
+                'get_object',
+                Params={
+                    'Bucket': bucket,
+                    'Key': key
+                },
+                ExpiresIn=3600
+            )
+            response_object['actions'] = {
+                operation: {
+                    'href': url,
+                    'expires_in': 3600
+                }
+            }
+            response['objects'].append(response_object)
+            continue
+
+        elif operation == 'download' and not object_exists:
+            # Return an error
+            pass
+
+        elif operation == 'upload' and object_exists:
+            response['objects'].append(response_object)
+            continue
+
+        elif operation == 'upload' and not object_exists:
+            url = s3.generate_presigned_url(
+                'put_object',
+                Params={
+                    'Bucket': bucket,
+                    'Key': key,
+
+                    # Git LFS includes a Content-Type header when it uses the basic
+                    # trasfer protocol for upload operations.  S3 presigned urls
+                    # consider this header in the signature so we need to include
+                    # it in the parameters.
+                    'ContentType': 'application/octet-stream',
+
+                    # This adds the x-amz-checksum-sha256 header, which tells
+                    # S3 to compute the sha256 checksum when the object
+                    # is uploaded.
+                    'ChecksumSHA256': b64_object_oid
+                },
+                ExpiresIn=3600
+            )
+            response_object['actions'] = {
+                operation: {
+                    'href': url,
+                    'header': {'x-amz-checksum-sha256': b64_object_oid},
+                    'expires_in': 3600
+                }
+            }
+            response['objects'].append(response_object)
+            continue
 
         else:
             raise Exception(
                 "event['operation'] needs to be either 'upload' or 'download'."
                 )
 
-        # The LFS API spec requires a download action in response to a
-        # download request.  If this isn't a download request (i.e. is an
-        # upload request) than an upload action is only necessary if
-        # the object doesn't already exist.
-        if operation == 'download' or not _object_exists(bucket, key):
-            url = s3.generate_presigned_url(
-                client_method,
-                Params=Params,
-                ExpiresIn=3600
-            )
-            response_object['actions'] = {
-                operation: {
-                    'href': url,
-                    # 'header': {},
-                    # 'expires_at': '2016-11-10T15:29:07Z',
-                    'expires_in': 3600
-                }
-            }
-
-        response['objects'].append(
-            response_object
-        )
+        response['objects'].append(response_object)
 
     return response
+
+
+def _hex_to_base64(hex):
+    return base64.b64encode(
+        bytes.fromhex(hex)).decode('utf-8')
 
 
 def _object_exists(bucket, key):
